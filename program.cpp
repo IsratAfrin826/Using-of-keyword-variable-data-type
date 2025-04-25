396aa8e0cf3d7084fca5eca26d5aed3a128c2e2c
Israt Arefin <nisratafrin826@gmail.com>
2025-04-25 10:37:57 +0600

    Add files via upload

---
+++ program.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+  float num1 = 15 , num2 = 23.5678 ;
+  char ch = 'i' ;
+
+  cout << "Number = " << num1 << endl << "Number = " << num2 << endl;
+  cout << "char = " << ch;
+
+  getch() ;
+}
